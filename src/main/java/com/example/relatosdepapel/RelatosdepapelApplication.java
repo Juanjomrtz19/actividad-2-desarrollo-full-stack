a2f33059e4c0ac5cd665fa8a578d5193a9e28b78
juanjomrtz19 <juanjodev19@gmail.com>
2026-02-03 23:25:10 +0100

    project initialized

---
+++ src/main/java/com/example/relatosdepapel/RelatosdepapelApplication.java
@@ -0,0 +1,13 @@
+package com.example.relatosdepapel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatosdepapelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatosdepapelApplication.class, args);
+	}
+
+}
