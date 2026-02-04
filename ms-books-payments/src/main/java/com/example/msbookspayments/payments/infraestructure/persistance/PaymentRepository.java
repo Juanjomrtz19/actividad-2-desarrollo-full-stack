615e3630aa8012e2c272f67282ee1c2d2467bb8d
Alex Cabeza <cabezacarsialejandro@gmail.com>
2026-02-04 12:48:23 +0100

    [FEAT] Add payment repository

---
+++ ms-books-payments/src/main/java/com/example/msbookspayments/payments/infraestructure/persistance/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.example.msbookspayments.payments.infraestructure.persistance;
+
+import com.example.msbookspayments.payments.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+}
