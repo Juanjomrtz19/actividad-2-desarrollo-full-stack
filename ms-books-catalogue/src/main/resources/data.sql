61518df8a94f7861c946ff1d51f6c5bcf5e2bb52
Alex Cabeza <cabezacarsialejandro@gmail.com>
2026-02-06 20:09:07 +0100

    [FEAT] v1.0

---
+++ ms-books-catalogue/src/main/resources/data.sql
@@ -0,0 +1,47 @@
+INSERT INTO books (title, author, publication_date, category, isbn_code, visibility, valoration)
+VALUES
+    (
+        'Cien años de soledad',
+        'Gabriel García Márquez',
+        '1967-05-30',
+        'Realismo mágico',
+        '9780307474728',
+        TRUE,
+        10
+    ),
+    (
+        '1984',
+        'George Orwell',
+        '1949-06-08',
+        'Distopía',
+        '9780451524935',
+        TRUE,
+        9
+    ),
+    (
+        'El nombre del viento',
+        'Patrick Rothfuss',
+        '2007-03-27',
+        'Fantasía',
+        '9788401352836',
+        FALSE,
+        9
+    ),
+    (
+        'La sombra del viento',
+        'Carlos Ruiz Zafón',
+        '2001-04-12',
+        'Misterio',
+        '9788408172178',
+        TRUE,
+        8
+    ),
+    (
+        'Sapiens: De animales a dioses',
+        'Yuval Noah Harari',
+        '2011-01-01',
+        'Ensayo histórico',
+        '9788499924212',
+        TRUE,
+        9
+    );
